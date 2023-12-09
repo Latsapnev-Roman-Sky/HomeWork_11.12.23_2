34a6e9520dda82fff88aaea201a9bc7801d47045
latsa <latsapnev01@mail.ru>
2023-12-09 18:34:30 +0300

    Task №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+
+    {   //Task 1
+        byte balls = 50;
+        short temperatureSpace = -270;
+        int companyIncomeYear = 1857732579;
+        long oneLightYear = 9460730472581L;
+        float accessiblePart = 0.25F;
+        double Pi = 3.1415926535897932;
+
+        System.out.println("Значение переменной balls с типом byte равно "+ balls);
+        System.out.println("Значение переменной temperatureSpace с типом short равно "+ temperatureSpace);
+        System.out.println("Значение переменной companyIncomeYear с типом int равно "+ companyIncomeYear);
+        System.out.println("Значение переменной oneLightYear с типом long равно "+ oneLightYear);
+        System.out.println("Значение переменной accessiblePart с типом float равно "+ accessiblePart);
+        System.out.println("Значение переменной Pi с типом double равно "+ Pi);
+
+
     }
 }
